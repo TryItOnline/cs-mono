2f4496b39470d66f934f2bbf282fb2f017fc0459
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2018-05-24 21:31:11 -0700

    Add Math.NET

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using MoreLinq;
 using Newtonsoft.Json.Linq;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.Expression;
 
 namespace TIO
 {
@@ -11,6 +13,8 @@ namespace TIO
         {
 			Console.WriteLine(MoreEnumerable.Random().First());
 			Console.WriteLine(JObject.Parse(@"{""foo"":42}"));
+			var x = Expr.Symbol("x");
+			Console.WriteLine(Infix.Format(x+x));
         }
     }
 }
