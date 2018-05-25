57671c76b47a6bbde07bc8c7a3559910f001be41
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2018-05-24 18:44:17 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using MoreLinq;
+using Newtonsoft.Json.Linq;
+
+namespace TIO
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine(MoreEnumerable.Random().First());
+			Console.WriteLine(JObject.Parse(@"{""foo"":42}"));
+        }
+    }
+}
