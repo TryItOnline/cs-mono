08342aef0aa9dd81b04cc507c4075574859d250d
pavelbraginskiy <pavelbraginskiy@hotmail.com>
2018-05-27 13:11:16 -0700

    Fix indentation in Program.cs

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ using Expr = MathNet.Symbolics.Expression;
 
 namespace TIO
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
 			Console.WriteLine(MoreEnumerable.Random().First());
 			Console.WriteLine(JObject.Parse(@"{""foo"":42}"));
 			var x = Expr.Symbol("x");
 			Console.WriteLine(Infix.Format(x+x));
-        }
-    }
+		}
+	}
 }
