d109df76c9bcccb0a9aa17a2d7cc211118adce72
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2018-05-26 21:52:27 -0700

    Fix AssemblyInfo for F# and VB.NET

--- Properties/AssemblyInfo.vb
+++ Properties/AssemblyInfo.vb
@@ -4,7 +4,7 @@ Imports System.Runtime.CompilerServices
 ' Information about this assembly is defined by the following attributes. 
 ' Change them to the values specific to your project.
 
-<assembly: AssemblyTitle("TIO-VB")>
+<assembly: AssemblyTitle("TIO")>
 <assembly: AssemblyDescription("")>
 <assembly: AssemblyConfiguration("")>
 <assembly: AssemblyCompany("")>
