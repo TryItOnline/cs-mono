270df33e351d1e88c24219468d600f08ddbd1fc4
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2018-07-05 12:46:06 -0700

    Add tests, fix F#

--- TIO-CS/Program.cs
+++ TIO-CS/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MoreLinq;
+using Newtonsoft.Json.Linq;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.Expression;
+using DynamicExpresso;
+using NDesk.Options;
 
-namespace cs_mono
+namespace TIO
 {
-	public static class Program
+	class MainClass
 	{
 		public static void Main(string[] args)
 		{
+			//MoreLinq
+			Console.WriteLine(MoreEnumerable.Random().First());
+			//Newtonsoft
+			Console.WriteLine(JObject.Parse(@"{""foo"":42}"));
+			//Mathnet
+			var x = Expr.Symbol("x");
+			Console.WriteLine(Infix.Format(x+x));
+			//DynamicExpresso
+			Console.WriteLine(new Interpreter().Eval("2+2"));
+			//NDesk.Options
+			Console.WriteLine(new OptionSet{
+				{"v", v=>{}}
+			}.Parse(new[] {"-v", "foo"})[0]);
+			//System.Memory
+			_ = new Span<int>();
 		}
 	}
 }
--- TIO-FS/Program.fs
+++ TIO-FS/Program.fs
@@ -1,7 +1,28 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open MoreLinq
+open System.Linq
+open MathNet.Symbolics
+open Newtonsoft.Json.Linq
+open DynamicExpresso
+open NDesk.Options
+
+open Operators
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+        //MoreLinq
+        MoreEnumerable.Random().First() |> printfn "%d"
+        //Newtonsoft
+        @"{""foo"":42}" |> JObject.Parse |> printfn "%O"
+        //Mathnet
+        let x = symbol "x"
+        x+x |> Infix.format |> printfn "%s" 
+        //DynamicExpresso
+        let interpreter = new Interpreter ()
+        "2+2" |> interpreter.Eval |> fun x -> x :?> int |> printfn "%d"
+        //NDesk.Options
+        let optionSet = new OptionSet()
+        optionSet.Add<string> ("v", (new Action<string>(fun x -> ()))) |> ignore
+        [|"-v"; "foo"|] |> optionSet.Parse |> printfn "%A"
+
+        0
